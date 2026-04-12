51f6b187290ef1a0582ff133a569013f1fb9a506
andi-huber <ahuber@apache.org>
2026-04-12 07:46:59 +0200

    adds open telemetry deps

--- domain/src/main/java/demoapp/dom/progmodel/actions/bulk/BulkActionPage.java
+++ domain/src/main/java/demoapp/dom/progmodel/actions/bulk/BulkActionPage.java
@@ -31,17 +31,19 @@ import jakarta.xml.bind.annotation.XmlType;
 
 import org.apache.causeway.applib.annotation.DomainObject;
 import org.apache.causeway.applib.annotation.DomainObjectLayout;
+import org.apache.causeway.applib.annotation.Introspection;
 import org.apache.causeway.applib.annotation.Nature;
 import org.apache.causeway.applib.annotation.ObjectSupport;
 
-import demoapp.dom._infra.asciidocdesc.HasAsciiDocDescription;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import demoapp.dom._infra.asciidocdesc.HasAsciiDocDescription;
+
 //tag::class[]
 @Named("demo.BulkActionPage")
-@DomainObject(nature=Nature.VIEW_MODEL)
+@DomainObject(nature=Nature.VIEW_MODEL, introspection = Introspection.ANNOTATION_OPTIONAL)
 @DomainObjectLayout(cssClassFa="fa-bolt")
 @XmlRootElement(name = "root")
 @XmlType
