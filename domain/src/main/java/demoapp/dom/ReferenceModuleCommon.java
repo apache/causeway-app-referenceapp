cc8fc177b072aef83c335f1eccf60f11d30fc1fd
andi-huber <ahuber@apache.org>
2026-05-20 07:34:20 +0200

    test approvals

--- domain/src/main/java/demoapp/dom/ReferenceModuleCommon.java
+++ domain/src/main/java/demoapp/dom/ReferenceModuleCommon.java
@@ -18,6 +18,11 @@
  */
 package demoapp.dom;
 
+//import java.util.List;
+//
+//import org.jspecify.annotations.Nullable;
+//
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -31,7 +36,18 @@ import org.apache.causeway.core.runtimeservices.CausewayModuleCoreRuntimeService
 import org.apache.causeway.extensions.docgen.help.CausewayModuleExtDocgenHelp;
 import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixturesApplib;
 
-@Configuration
+//import io.micrometer.core.instrument.Tags;
+//import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+//import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+//import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmClassLoadingMeterConventions;
+//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmCpuMeterConventions;
+//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmMemoryMeterConventions;
+//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmThreadMeterConventions;
+//import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+//import io.micrometer.registry.otlp.OtlpConfig;
+
+@Configuration(proxyBeanMethods = false)
 @Import({
     CausewayModuleApplibChangeAndExecutionLoggers.class,
     CausewayModuleApplibMixins.class,
@@ -48,5 +64,39 @@ import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixtures
                 ReferenceModuleCommon.class
         })
 public class ReferenceModuleCommon {
+
+//OpenTelemetry demo
+//    @Bean
+//    OtlpConfig otlpConfig() {
+//        return new OtlpConfig() {
+//            @Override public @Nullable String get(final String key) { return null;}
+//            @Override public boolean enabled() { return false; }
+//        };
+//    }
+//
+//    @Bean
+//    OpenTelemetryJvmCpuMeterConventions openTelemetryJvmCpuMeterConventions() {
+//        return new OpenTelemetryJvmCpuMeterConventions(Tags.empty());
+//    }
+//
+//    @Bean
+//    ProcessorMetrics processorMetrics() {
+//        return new ProcessorMetrics(List.of(), new OpenTelemetryJvmCpuMeterConventions(Tags.empty()));
+//    }
+//
+//    @Bean
+//    JvmMemoryMetrics jvmMemoryMetrics() {
+//        return new JvmMemoryMetrics(List.of(), new OpenTelemetryJvmMemoryMeterConventions(Tags.empty()));
+//    }
+//
+//    @Bean
+//    JvmThreadMetrics jvmThreadMetrics() {
+//        return new JvmThreadMetrics(List.of(), new OpenTelemetryJvmThreadMeterConventions(Tags.empty()));
+//    }
+//
+//    @Bean
+//    ClassLoaderMetrics classLoaderMetrics() {
+//        return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
+//    }
 
 }
