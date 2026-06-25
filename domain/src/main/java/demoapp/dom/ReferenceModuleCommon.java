04cff95802de44d0ff5ac0802857f3bb2ffe8c3a
ahuber@apache.org <ahuber@apache.org@192.168.0.235>
2026-06-25 13:04:54 +0200

    adds 'euro' value semantics for demo

--- domain/src/main/java/demoapp/dom/ReferenceModuleCommon.java
+++ domain/src/main/java/demoapp/dom/ReferenceModuleCommon.java
@@ -18,34 +18,38 @@
  */
 package demoapp.dom;
 
-//import java.util.List;
-//
-//import org.jspecify.annotations.Nullable;
-//
-//import org.springframework.context.annotation.Bean;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.stereotype.Component;
 
 import org.apache.causeway.applib.CausewayModuleApplibChangeAndExecutionLoggers;
 import org.apache.causeway.applib.CausewayModuleApplibMixins;
 import org.apache.causeway.core.config.presets.CausewayPresets;
+import org.apache.causeway.core.metamodel.valuesemantics.BigDecimalValueSemantics;
 import org.apache.causeway.core.runtimeservices.CausewayModuleCoreRuntimeServices;
 import org.apache.causeway.extensions.docgen.help.CausewayModuleExtDocgenHelp;
 import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixturesApplib;
 
-//import io.micrometer.core.instrument.Tags;
-//import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-//import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-//import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmClassLoadingMeterConventions;
-//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmCpuMeterConventions;
-//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmMemoryMeterConventions;
-//import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmThreadMeterConventions;
-//import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-//import io.micrometer.registry.otlp.OtlpConfig;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmClassLoadingMeterConventions;
+import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmCpuMeterConventions;
+import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmMemoryMeterConventions;
+import io.micrometer.core.instrument.binder.jvm.convention.otel.OpenTelemetryJvmThreadMeterConventions;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.registry.otlp.OtlpConfig;
 
 @Configuration(proxyBeanMethods = false)
 @Import({
@@ -65,38 +69,60 @@ import org.apache.causeway.testing.fixtures.applib.CausewayModuleTestingFixtures
         })
 public class ReferenceModuleCommon {
 
+    /**
+     * value semantics for demo
+     *
+     * <p>Example usage:
+     * <pre>@ValueSemantics(provider="euro", maxIntegerDigits = 4, minFractionalDigits = 2, maxFractionalDigits = 2)</pre>
+     */
+    @Component
+    @Qualifier("euro")
+    public static class BigDecimalEuro extends BigDecimalValueSemantics.LocaleGroupingAll {
+        @Override public String htmlPresentation(final Context context, final BigDecimal value) {
+            return renderTitle(value, pipe(
+                getNumberFormat(context, FormatUsageFor.RENDERING_AS_HTML)::format,
+                html->value.signum() == -1
+                    ? toLightFontWithStyle(html, "color: red")
+                    : toLightFontWithStyle(html, "")));
+        }
+        private final String toLightFontWithStyle(final String html, final String style) {
+            return """
+                <span class="fw-light" style="%s">€ %s</span>""".formatted(style, html);
+        }
+    }
+
 //OpenTelemetry demo
-//    @Bean
-//    OtlpConfig otlpConfig() {
-//        return new OtlpConfig() {
-//            @Override public @Nullable String get(final String key) { return null;}
-//            @Override public boolean enabled() { return false; }
-//        };
-//    }
-//
-//    @Bean
-//    OpenTelemetryJvmCpuMeterConventions openTelemetryJvmCpuMeterConventions() {
-//        return new OpenTelemetryJvmCpuMeterConventions(Tags.empty());
-//    }
-//
-//    @Bean
-//    ProcessorMetrics processorMetrics() {
-//        return new ProcessorMetrics(List.of(), new OpenTelemetryJvmCpuMeterConventions(Tags.empty()));
-//    }
-//
-//    @Bean
-//    JvmMemoryMetrics jvmMemoryMetrics() {
-//        return new JvmMemoryMetrics(List.of(), new OpenTelemetryJvmMemoryMeterConventions(Tags.empty()));
-//    }
-//
-//    @Bean
-//    JvmThreadMetrics jvmThreadMetrics() {
-//        return new JvmThreadMetrics(List.of(), new OpenTelemetryJvmThreadMeterConventions(Tags.empty()));
-//    }
-//
-//    @Bean
-//    ClassLoaderMetrics classLoaderMetrics() {
-//        return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
-//    }
+    @Bean
+    OtlpConfig otlpConfig() {
+        return new OtlpConfig() {
+            @Override public @Nullable String get(final String key) { return null;}
+            @Override public boolean enabled() { return false; }
+        };
+    }
+
+    @Bean
+    OpenTelemetryJvmCpuMeterConventions openTelemetryJvmCpuMeterConventions() {
+        return new OpenTelemetryJvmCpuMeterConventions(Tags.empty());
+    }
+
+    @Bean
+    ProcessorMetrics processorMetrics() {
+        return new ProcessorMetrics(List.of(), new OpenTelemetryJvmCpuMeterConventions(Tags.empty()));
+    }
+
+    @Bean
+    JvmMemoryMetrics jvmMemoryMetrics() {
+        return new JvmMemoryMetrics(List.of(), new OpenTelemetryJvmMemoryMeterConventions(Tags.empty()));
+    }
+
+    @Bean
+    JvmThreadMetrics jvmThreadMetrics() {
+        return new JvmThreadMetrics(List.of(), new OpenTelemetryJvmThreadMeterConventions(Tags.empty()));
+    }
+
+    @Bean
+    ClassLoaderMetrics classLoaderMetrics() {
+        return new ClassLoaderMetrics(new OpenTelemetryJvmClassLoadingMeterConventions());
+    }
 
 }
